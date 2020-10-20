63697e8fb8c326bca12bd6027c1a6b5b24fb84bb
guilherme <guilherme@campusinc.com.br>
2020-10-20 01:00:50 -0300

    add interface to errors

--- utils/errors/rest_errors.go
+++ utils/errors/rest_errors.go
@@ -2,33 +2,44 @@ package errors
 
 import "net/http"
 
+var (
+	ErrorService errorServiceInterface = &errorRestService{}
+)
+
+type errorRestService struct{}
+
+type errorServiceInterface interface {
+	BadRequestErr(string) *RestErr
+	InternalServerError(string) *RestErr
+	NotFound(string) *RestErr
+}
+
 type RestErr struct {
 	Message string `json:"message"`
-	Status int `json:"status"`
-	Error string `json:"error"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
 }
 
-
-func BadRequestErr (msg string) *RestErr{
+func (e *errorRestService) BadRequestErr(msg string) *RestErr {
 	return &RestErr{
 		Message: msg,
-		Status: http.StatusBadRequest,
-		Error: "bad_request",
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
 	}
 }
 
-func InternalServerError (msg string) *RestErr{
+func (e *errorRestService) InternalServerError(msg string) *RestErr {
 	return &RestErr{
 		Message: msg,
-		Status: http.StatusInternalServerError,
-		Error: "internal_server_error",
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
 	}
 }
 
-func NotFound (msg string) *RestErr{
+func (e *errorRestService) NotFound(msg string) *RestErr {
 	return &RestErr{
 		Message: msg,
-		Status: http.StatusNotFound,
-		Error: "not_found",
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
 	}
 }
